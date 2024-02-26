007247e6d60d237955bfced8ba3979b565f05b18
aksoft <aksoft00@gmail.com>
2024-02-26 19:00:13 +0530

    TextMeshPro and Unity UI Imported

--- Assets/Circle/Scripts/GameManager.cs
+++ Assets/Circle/Scripts/GameManager.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
-    public ClientDataManager clientDataManager;
 
     [SerializeField] Button restartButton;
 
-    public UnityEvent<ClientDataManager.ClientEntry> onShowClientDetails;
 
     public static GameManager instance;
 
@@ -22,7 +19,6 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this);
         }
-        onShowClientDetails = new UnityEvent<ClientDataManager.ClientEntry>();
         restartButton.onClick.RemoveAllListeners();
         restartButton.onClick.AddListener(RestartGame);
     }
